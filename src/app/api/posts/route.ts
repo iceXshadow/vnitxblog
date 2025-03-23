628584c86516ba0d4e539b4478e6fc67e0ef4f81
iceXshadow <devxicy@gmail.com>
2025-03-23 14:04:08 +0530

    fix error deploy

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/lib/connectDB";
 import Post from "@/models/Posts";
